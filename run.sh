f3a067980204b13dae7e7f045a7155e5170ecdf2
Justin Kelly <justin@kelly.org.au>
2016-04-18 12:52:53 +1000

    pid 1 issue

--- run.sh
+++ run.sh
@@ -10,6 +10,9 @@ else
     a2enmod rewrite
 fi
 
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
+
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
