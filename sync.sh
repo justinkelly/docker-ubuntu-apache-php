870d4eda5542ded7225f1bc570b7cf756dac610b
Justin Kelly <justin@kelly.org.au>
2016-09-13 12:35:40 +1000

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if ["$@" == 'up']
-then
+if [ $1 = "up"] ; then
   /s3 --region "${AWS_REGION}" sync /app/ s3://${AWS_BUCKET}/
 else
   /s3 --region "${AWS_REGION}" sync s3://${AWS_BUCKET}/ /app/
