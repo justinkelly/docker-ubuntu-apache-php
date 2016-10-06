a68f5e8e7d52c48ef5302aa83713aab48af15129
Justin Kelly <justin@kelly.org.au>
2016-10-06 16:20:54 +1100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ $1 = "up" ] ; then
-  /s3 --region "${AWS_REGION}" sync /app/ s3://${AWS_BUCKET}/
+  #/s3 --region "${AWS_REGION}" sync /app/ s3://${AWS_BUCKET}/
+  /mc mirror /app/ myminio://${AWS_BUCKET}/
 else
-  /s3 --region "${AWS_REGION}" sync s3://${AWS_BUCKET}/ /app/
+  #/s3 --region "${AWS_REGION}" sync s3://${AWS_BUCKET}/ /app/
+  /mc mirror myminio/${AWS_BUCKET}/ /app/
 fi
